8e4b6e5bba0503c3d5ba6b103f7ed3369cf8fe3d
Chetan3327 <chauhanchetan12789@gmail.com>
2025-10-24 23:03:00 +0530

    24.10.25

---
+++ G_Bicycles.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define ld long double
+#define endl "\n"
+#define sp " "
+#define testcases ll ts; cin >> ts; for(ll tsno = 1; tsno <= ts; tsno++)
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+template <typename T> void input(vector<T> &a){for(auto &e: a) cin >> e;}
+template <typename T> void input(vector<vector<T>> &a){for(auto &r: a) for(auto &c: r) cin >> c;}
+template <typename T> void print(T ans){cout<<ans<<"\n";}
+template <typename T> void print(vector <T> &ans){for(auto &i : ans) cout<<i<<" "; cout<<"\n";}
+template <typename T> void print(vector <vector<T>> &ans){for(auto &i : ans) {{for(auto &j: i) cout<<j<<" ";} cout<<"\n";}}
+#define MOD 1000000007
+ll floor(ll a, ll b) {ll c = a / b; if (a % b != 0 && ((a < 0 && b > 0) || (a > 0 && b < 0))) c--; return c;}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll mod_add(ll a, ll b, ll m){a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m){a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m){a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}
+ll ceil_div(ll a, ll b) {return a / b + ((a ^ b) > 0 && a % b != 0);}
+vector<pair<ll, ll>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+void solve(){
+  ll n, m;
+  cin >> n >> m;
+
+  vector<vector<pair<ll, ll>>> adj(n);
+  for(int i = 0; i < m; i++){
+    ll u, v, w;
+    cin >> u >> v >> w;
+    u--, v--;
+    adj[u].push_back({v, w});
+    adj[v].push_back({u, w});
+  }
+
+  vll s(n);
+  input(s);
+
+  vvll dist(n, vll(n, 1e18));
+  dist[0][0] = 0;
+
+  vvll vis(n, vll(n, 0));
+
+  priority_queue<pair<ll, pair<ll, ll>>, vector<pair<ll, pair<ll, ll>>>, greater<pair<ll, pair<ll, ll>>>> pq;
+  pq.push({0, {0, 0}});
+
+  while(!pq.empty()){
+    ll curr = pq.top().first;
+    ll city = pq.top().second.first;
+    ll bike = pq.top().second.second;
+    pq.pop();
+
+    if(city == n - 1){
+      print(dist[n - 1][bike]);
+      return ;
+    }
+
+    if(vis[city][bike]) continue;
+    vis[city][bike] = 1;
+
+    for(auto itr: adj[city]){
+      ll adjnode = itr.first;
+      ll wt = itr.second;
+
+      ll newbike = bike;
+      if(s[adjnode] < s[bike]) newbike = adjnode;
+
+      if(dist[adjnode][newbike] < dist[city][bike] + wt * s[bike]) continue;
+      dist[adjnode][newbike] = dist[city][bike] + wt * s[bike];
+      pq.push({dist[adjnode][newbike], {adjnode, newbike}});
+    }
+  }
+
+  print(-1);
+}
+int main(){
+  fastio
+  testcases{
+    solve();
+  }
+}
